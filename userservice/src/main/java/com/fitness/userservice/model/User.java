3ebe448ac5d06c548d41a74956c56daf867cfa33
unknown <charnel.dolon@gmail.com>
2026-03-27 14:11:51 -0400

    User file

---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,38 @@
+package com.fitness.userservice.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(jakarta.persistence.EnumType.STRING)
+    private UserRole role = UserRole.USER;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
