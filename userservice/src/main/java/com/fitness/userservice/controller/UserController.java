6d58b6340beb344225919f64641e785086da2eee
unknown <charnel.dolon@gmail.com>
2026-03-31 22:34:51 -0400

    UserService

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,22 +1,28 @@
 package com.fitness.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{userId}")
-    public ResponseEntity<UserResponse> getUserProile(@PathVariable String userId) {
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
         return ResponseEntity.ok(userService.getUserProfile(userId));
     }
 
     @GetMapping("/register")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
         return ResponseEntity.ok(userService.register(request));
     }
 
