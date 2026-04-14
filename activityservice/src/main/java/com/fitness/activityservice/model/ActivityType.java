02e7307913ee591503f88412628466e49f4ce72a
unknown <charnel.dolon@gmail.com>
2026-04-13 23:00:36 -0400

    Activity Service setup

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHTLIFTING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    BASKETBALL,
+}
